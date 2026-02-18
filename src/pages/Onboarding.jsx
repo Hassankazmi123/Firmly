f41675fafae1789f8a819cef94c13d017a78fa16
rimshakanwal00 <rimshakanwal098765@gmail.com>
2026-02-18 23:08:52 +0500

    fix: fix issues of routes refresh

--- src/pages/Onboarding.jsx
+++ src/pages/Onboarding.jsx
@@ -1,17 +1,71 @@
+// import React, { useState, useEffect } from "react";
+// import OnboardingLayout from "../components/OnboardingComponents/OnboardingLayout";
+// import IntroCard from "../components/OnboardingComponents/IntroCard";
+// import PathwayCard from "../components/OnboardingComponents/PathwayCard";
+// import Screen3Card from "../components/OnboardingComponents/Screen3Card";
+// import Screen4Card from "../components/OnboardingComponents/Screen4Card";
+// import clearAppCache from "../utils/cache";
+
+// const Onboarding = () => {
+//   const [currentScreen, setCurrentScreen] = useState(0);
+
+//   useEffect(() => {
+//     clearAppCache().catch((e) => console.warn("clearAppCache error:", e));
+//   }, []);
+
+//   const handleSetScreen = (screenIndex) => {
+//     setCurrentScreen(screenIndex);
+//   };
+
+//   const screens = [
+//     {
+//       component: <IntroCard onNext={() => handleSetScreen(1)} />,
+//     },
+//     {
+//       component: (
+//         <PathwayCard
+//           onPrevious={() => handleSetScreen(0)}
+//           onNext={() => handleSetScreen(2)}
+//         />
+//       ),
+//     },
+//     {
+//       component: (
+//         <Screen3Card
+//           onPrevious={() => handleSetScreen(1)}
+//           onNext={() => handleSetScreen(3)}
+//           onLogin={() => handleSetScreen(3)}
+//         />
+//       ),
+//     },
+//     {
+//       component: <Screen4Card onBack={() => handleSetScreen(2)} />,
+//     },
+//   ];
+
+//   return (
+//     <OnboardingLayout>{screens[currentScreen]?.component}</OnboardingLayout>
+//   );
+// };
+
+// export default Onboarding;
+
 import React, { useState, useEffect } from "react";
 import OnboardingLayout from "../components/OnboardingComponents/OnboardingLayout";
 import IntroCard from "../components/OnboardingComponents/IntroCard";
 import PathwayCard from "../components/OnboardingComponents/PathwayCard";
 import Screen3Card from "../components/OnboardingComponents/Screen3Card";
 import Screen4Card from "../components/OnboardingComponents/Screen4Card";
-import clearAppCache from "../utils/cache";
 
 const Onboarding = () => {
-  const [currentScreen, setCurrentScreen] = useState(0);
+  const [currentScreen, setCurrentScreen] = useState(() => {
+    const savedStep = localStorage.getItem("onboardingStep");
+    return savedStep ? Number(savedStep) : 0;
+  });
 
   useEffect(() => {
-    clearAppCache().catch((e) => console.warn("clearAppCache error:", e));
-  }, []);
+    localStorage.setItem("onboardingStep", currentScreen);
+  }, [currentScreen]);
 
   const handleSetScreen = (screenIndex) => {
     setCurrentScreen(screenIndex);
